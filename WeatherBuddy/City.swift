0b8edca6a8c3237cf339e960fe0f0eb39ea84f55
mwalsh29 <mwalsh29@nd.edu>
2016-03-30 16:46:20 -0400

    added City class

---
+++ WeatherBuddy/City.swift
@@ -0,0 +1,24 @@
+//
+//  City.swift
+//  WeatherBuddy
+//
+//  Created by Max Walsh on 3/30/16.
+//  Copyright © 2016 Katie Kuenster. All rights reserved.
+//
+
+import Foundation
+
+
+class City {
+    var currentTemp:Double
+    var humidity:Int
+    var description:String
+    var minTemp:Double
+    var maxTemp:Double
+    var windSpeed:Double
+    var windDirection:Double
+    var rain:String
+    var sunrise:Int
+    var sunset:Int
+    
+}
