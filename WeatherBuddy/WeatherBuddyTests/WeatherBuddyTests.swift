e1c62f247093b50077938db86afb1c9d468d2c77
Katie Kuenster <katiekuenster@katiekustersair.dhcp.nd.edu>
2016-03-30 16:28:53 -0400

    initial commit

---
+++ WeatherBuddy/WeatherBuddyTests/WeatherBuddyTests.swift
@@ -0,0 +1,36 @@
+//
+//  WeatherBuddyTests.swift
+//  WeatherBuddyTests
+//
+//  Created by Katie Kuenster on 3/30/16.
+//  Copyright © 2016 Katie Kuenster. All rights reserved.
+//
+
+import XCTest
+@testable import WeatherBuddy
+
+class WeatherBuddyTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
