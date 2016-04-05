3edb34625017224cf2af01de5a7a96a07738cab7
mwalsh29 <mwalsh29@nd.edu>
2016-04-05 12:54:02 -0400

    fixed spacing

--- WeatherBuddy/OpenWeatherService.swift
+++ WeatherBuddy/OpenWeatherService.swift
@@ -71,25 +71,24 @@ class OpenWeatherService {
         let session = NSURLSession.sharedSession()
         let task = session.dataTaskWithRequest(request) {
         (data, responseText, error) -> Void in
-        if error != nil {
-        print(error)
-        } else {
-        let result = String(data: data!, encoding: NSASCIIStringEncoding)!
+            if error != nil {
+                print(error)
+            } else {
+                let result = String(data: data!, encoding: NSASCIIStringEncoding)!
         
-        // http://stackoverflow.com/questions/24056205/how-to-use-background-thread-in-swift
-        let qualityOfServiceClass = QOS_CLASS_BACKGROUND
-        let backgroundQueue = dispatch_get_global_queue(qualityOfServiceClass, 0)
+                // http://stackoverflow.com/questions/24056205/how-to-use-background-thread-in-swift
+                let qualityOfServiceClass = QOS_CLASS_BACKGROUND
+                let backgroundQueue = dispatch_get_global_queue(qualityOfServiceClass, 0)
         
-        //dispatch_async(dispatch_get_main_queue(), {
-        dispatch_async(backgroundQueue, {
-        self.resultJSON = result
-        self.parseJSONCoordResponse(data!)
-        callback(cities)
-        })
-        }
+                //dispatch_async(dispatch_get_main_queue(), {
+                dispatch_async(backgroundQueue, {
+                    self.resultJSON = result
+                    self.parseJSONCoordResponse(data!)
+                    callback(cities)
+                })
+            }
         }
         task.resume()
-        
     }
     
     
